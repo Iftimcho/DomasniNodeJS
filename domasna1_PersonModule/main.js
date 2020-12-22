48bb6e4eac4cb44dbf5f31510af9d6589183ccb4
DESKTOP-UQ7FNL2\Timce <timcepop1@yahoo.com>
2020-12-22 21:36:26 +0100

    Ja premestiv proverkata za polnoleten od main.js vo person.js

--- domasna1_PersonModule/main.js
+++ domasna1_PersonModule/main.js
@@ -14,13 +14,21 @@ const person2 = {
     city: 'Kavadarci'
 }
 
+// Gi povikav funkciite na ovoj nacin. Mislam deka e podobro da si gi povikam kako funkcii i da si gi zapisam vo promenliva, pa potoa da si ja koristam samo promenlivata.
+// Toa bi go iskoristil dokolku treba mnogu pati da ja povikam nekoja funkcija. Vo ovoj slucaj gi povikuvam samo po ednas..
+
 const fullName = personModule.getFullName(person1);
 console.log(fullName);
 
 const personData = personModule.getData(person2);
 console.log(personData);
 
-const isAdult = personModule.isAdult(person2);
+const isAdult = personModule.isAdult(person1);
+console.log(isAdult);
+/*
+
+Go promeniv kodot ovaa proverka ja staviv vo person modulot
+
 if(isAdult === true)
 {
     console.log('Polnoleten');
@@ -29,3 +37,4 @@ else
 {
     console.log('Maloleten');
 }
+*/
