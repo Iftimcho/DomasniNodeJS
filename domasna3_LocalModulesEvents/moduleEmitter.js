78112d26a70080118ace6a1d4992dba6d30104a3
Iftimcho <70629118+Iftimcho@users.noreply.github.com>
2020-12-28 22:41:01 +0100

    Code readibility fixed

--- domasna3_LocalModulesEvents/moduleEmitter.js
+++ domasna3_LocalModulesEvents/moduleEmitter.js
@@ -3,8 +3,7 @@ const EventEmitter = require('events');
 // isntanca od klasata za da koristime metodi od nea
 
 class Test extends EventEmitter {
-    sendMessage(message)
-    {
+    sendMessage(message) {
         this.emit('room21', message);
     }
 }
